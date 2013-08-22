b3fb08fb42857e0f037db5f5251f7920b0b9633d
aaron.dai <aaronday.china@gmail.com>
2013-08-22 20:54:26 +0800

    8/22 add topic detail page

--- sites/all/themes/dxf/templates/page.tpl.php
+++ sites/all/themes/dxf/templates/page.tpl.php
@@ -9,25 +9,25 @@
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-        <link rel="stylesheet" href="<?php echo drupal_get_path('theme', 'dxf')?>/css/reveal.css">
-        <link rel="stylesheet" href="<?php echo drupal_get_path('theme', 'dxf')?>/css/theme/default.css" id="theme">
+        <link rel="stylesheet" href="/dxf/<?php echo drupal_get_path('theme', 'dxf')?>/css/reveal.css">
+        <link rel="stylesheet" href="/dxf/<?php echo drupal_get_path('theme', 'dxf')?>/css/theme/default.css" id="theme">
 
         <!-- For syntax highlighting -->
-        <link rel="stylesheet" href="<?php echo drupal_get_path('theme', 'dxf')?>/lib/css/zenburn.css">
+        <link rel="stylesheet" href="/dxf/<?php echo drupal_get_path('theme', 'dxf')?>/lib/css/zenburn.css">
 
         <!--<link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic' rel='stylesheet' type='text/css'>-->
 
-        <link rel="stylesheet" href="<?php echo drupal_get_path('theme', 'dxf')?>/css/style.css">
+        <link rel="stylesheet" href="/dxf/<?php echo drupal_get_path('theme', 'dxf')?>/css/style.css">
 
         <!-- If the query includes 'print-pdf', use the PDF print sheet -->
         <script>
-            document.write('<link rel="stylesheet" href="<?php echo drupal_get_path('theme', 'dxf')?>/css/print/' + (window.location.search.match(/print-pdf/gi) ? 'pdf' : 'paper') + '.css" type="text/css" media="print">');
+            document.write('<link rel="stylesheet" href="/dxf/<?php echo drupal_get_path('theme', 'dxf')?>/css/print/' + (window.location.search.match(/print-pdf/gi) ? 'pdf' : 'paper') + '.css" type="text/css" media="print">');
         </script>
 
         <!--[if lt IE 9]>
         <script src="lib/js/html5shiv.js"></script>
         <![endif]-->
-        <link rel="stylesheet" href="<?php echo drupal_get_path('theme', 'dxf')?>/css/forkit.css">
+        <link rel="stylesheet" href="/dxf/<?php echo drupal_get_path('theme', 'dxf')?>/css/forkit.css">
     </head>
 
     <body>
@@ -35,9 +35,9 @@
         <div class="meny">
             <h2>[ AARON-PHOTOGRAPHY ]</h2>
             <ul>
-                <li><a href="home">HOME</a></li>
-                <li><a href="imagewall">IMAGE GALLERY WALL</a></li>
-                <li><a href="topic">FEATURED TOPIC</a></li>
+                <li><a href="/dxf/home">HOME</a></li>
+                <li><a href="/dxf/imagewall">IMAGE GALLERY WALL</a></li>
+                <li><a href="/dxf/topic">FEATURED TOPIC</a></li>
             </ul>
         </div>
 
@@ -46,10 +46,10 @@
         <div class="contents">
             <?php print $page['content']['system_main']['main']['#markup']; ?>
             <a class="forkit" data-text="Follow Me" data-text-detached="Click On >" target="_blank" href="http://weibo.com/aaronlovequeena"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/camo.github.com/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
-            <script src="<?php echo drupal_get_path('theme', 'dxf')?>/js/forkit.js"></script>
+            <script src="/dxf/<?php echo drupal_get_path('theme', 'dxf')?>/js/forkit.js"></script>
         </div>
 
-        <script src="<?php echo drupal_get_path('theme', 'dxf')?>/js/meny.min.js"></script>
+        <script src="/dxf/<?php echo drupal_get_path('theme', 'dxf')?>/js/meny.min.js"></script>
         <script>
             // Create an instance of Meny
             var meny = Meny.create({
