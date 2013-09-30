55f249e1bd9f638c59b66f661b8d5fda26ffc413
aaron.dai <aaron.dai@perficientgdc.com.cn>
2013-09-30 16:48:06 +0800

    Sep 30-2

--- sites/all/themes/dxf/templates/page--mc--login.tpl.php
+++ sites/all/themes/dxf/templates/page--mc--login.tpl.php
@@ -4,9 +4,9 @@
 </div>
 <div id="page-wrapper">
     <div id="page">
-        <div id="main-wrapper" class="clearfix" style="width: 800px; margin: 0px auto; padding-top: 130px;">
-            <div class="mc-login-title" style="width: 600px; margin: 0px auto;"><img src="/dxf/<?php echo drupal_get_path('theme', 'dxf') ?>/images/mc-login-title.png" width="600px" height="180px"/></div>
-            <div id="main" class="clearfix">
+        <div id="main-wrapper" class="clearfix" style="width: 800px; margin: 0px auto; padding-top: 180px;">
+            <div class="mc-login-title" style="width: 750px; margin: 0px auto;">点滴·记录·片刻·永恒</div><!-- <img src="/dxf/<?php echo drupal_get_path('theme', 'dxf') ?>/images/mc-login-title.png" width="600px" height="180px"/> -->
+            <div id="main" class="clearfix" style="margin-top: 50px;">
                 <div id="content" class="column">
                     <div class="section">
                         <?php print render($page['login_block']); ?>
